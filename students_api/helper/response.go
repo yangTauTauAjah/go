980dfdace76e6e8d1748ca04704c5968c90da655
Habib Anwash <tesakun6464@gmail.com>
2026-09-10 12:47:28 +0700

    feat: add response helper functions for standardized API responses

---
+++ students_api/helper/response.go
@@ -0,0 +1,42 @@
+package helper
+
+import (
+	"tugas2/app/model"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Success(c *fiber.Ctx, status int, message string, data any) error {
+	return c.Status(status).JSON(model.WebResponse{
+		Success: true, Message: message, Data: data,
+	})
+}
+
+func SuccessList(c *fiber.Ctx, message string, data any, meta *model.Meta) error {
+	return c.Status(fiber.StatusOK).JSON(model.WebResponse{
+		Success: true, Message: message, Data: data, Meta: meta,
+	})
+}
+
+func Created(c *fiber.Ctx, message string, data any, location string) error {
+	c.Set("Location", location)
+	return c.Status(fiber.StatusCreated).JSON(model.WebResponse{
+		Success: true, Message: message, Data: data,
+	})
+}
+
+func NoContent(c *fiber.Ctx) error {
+	return c.SendStatus(fiber.StatusNoContent)
+}
+
+func Fail(c *fiber.Ctx, status int, message string) error {
+	return c.Status(status).JSON(model.WebResponse{
+		Success: false, Message: message,
+	})
+}
+
+func FailValidation(c *fiber.Ctx, errs map[string]string) error {
+	return c.Status(fiber.StatusUnprocessableEntity).JSON(model.WebResponse{
+		Success: false, Message: "validasi gagal", Errors: errs,
+	})
+}
