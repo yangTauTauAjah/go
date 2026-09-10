4cacd034036817c6f695469d7082016073cd0e43
Habib Anwash <tesakun6464@gmail.com>
2026-09-10 12:47:40 +0700

    feat: implement middleware for request handling and logging

---
+++ students_api/middleware/middleware.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"log/slog"
+	"strings"
+	"time"
+	"tugas2/helper"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+)
+
+func Register(app *fiber.App, logger *slog.Logger) {
+	app.Use(requestid.New())       // 1. beri setiap request satu ID unik
+	app.Use(recover.New())         // 2. tangkap panic agar server tidak mati
+	app.Use(helmet.New())          // 3. pasang header keamanan dasar
+	app.Use(cors.New())            // 4. atur Cross-Origin Resource Sharing
+	app.Use(RequestLogger(logger)) // 5. catat setiap request
+}
+
+func RequestLogger(logger *slog.Logger) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next()
+		requestID, _ := c.Locals("requestid").(string)
+		logger.Info("http_request",
+			slog.String("request_id", requestID),
+			slog.String("method", c.Method()),
+			slog.String("path", c.Path()),
+			slog.Int("status", c.Response().StatusCode()),
+			slog.Duration("duration", time.Since(start)),
+			slog.String("ip", c.IP()),
+		)
+		return err
+	}
+}
+
+var methodsWithBody = map[string]bool{
+	fiber.MethodPost:  true,
+	fiber.MethodPut:   true,
+	fiber.MethodPatch: true,
+}
+
+func RequireJSON(c *fiber.Ctx) error {
+	if methodsWithBody[c.Method()] {
+		ct := c.Get("Content-Type")
+		if !strings.HasPrefix(ct, fiber.MIMEApplicationJSON) {
+			return helper.Fail(c, fiber.StatusUnsupportedMediaType,
+				"Content-Type harus application/json")
+		}
+	}
+	return c.Next()
+}
