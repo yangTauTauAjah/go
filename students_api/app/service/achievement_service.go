34f38d8c2ae0dfe42563d1edc9ab8e98d6821ec8
Habib Anwash <tesakun6464@gmail.com>
2026-09-16 07:53:33 +0700

    feat: implement AchievementService with List and Get methods

---
+++ students_api/app/service/achievement_service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"tugas2/app/model"
+	"tugas2/app/repository"
+	"tugas2/helper"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type AchievementService struct {
+	repo repository.AchievementRepository
+}
+
+func NewAchievementService(repo repository.AchievementRepository) *AchievementService {
+	return &AchievementService{repo: repo}
+}
+
+func (s *AchievementService) List(c *fiber.Ctx) error {
+	ctx, cancel := helper.RequestContext(c)
+	defer cancel()
+	q := helper.ParseListQuery(c)
+	achievements, total, err := s.repo.FindAll(ctx, q)
+	if err != nil {
+		return helper.Fail(c, fiber.StatusInternalServerError,
+			"gagal mengambil data achievement")
+	}
+	return helper.SuccessList(c, "daftar achievement berhasil diambil", achievements, &model.Meta{
+		Page:       q.Page,
+		Limit:      q.Limit,
+		Total:      total,
+		TotalPages: CountTotalPages(total, q.Limit),
+	})
+}
+
+func (s *AchievementService) Get(c *fiber.Ctx) error {
+	ctx, cancel := helper.RequestContext(c)
+	defer cancel()
+	id, valid := helper.ParamID(c)
+	if !valid {
+		return helper.Fail(c, fiber.StatusBadRequest, "id harus berupa angka positif")
+	}
+	achievement, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		return translateError(c, err, "gagal mengambil data achievement")
+	}
+	return helper.Success(c, fiber.StatusOK, "achievement ditemukan", achievement)
+}
