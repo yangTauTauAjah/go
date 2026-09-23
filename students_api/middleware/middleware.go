c9cff69e344d91198b4143a17b6bbdd47ce4ada6
Habib Anwash <tesakun6464@gmail.com>
2026-09-24 05:36:04 +0700

    feat: enhance RequestLogger to include user information in logs

--- students_api/middleware/middleware.go
+++ students_api/middleware/middleware.go
@@ -23,17 +23,26 @@ func Register(app *fiber.App, logger *slog.Logger, allowedOrigins string) {
 
 func RequestLogger(logger *slog.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+
 		start := time.Now()
 		err := c.Next()
 		requestID, _ := c.Locals("requestid").(string)
-		logger.Info("http_request",
+
+		attrs := []any{
 			slog.String("request_id", requestID),
 			slog.String("method", c.Method()),
 			slog.String("path", c.Path()),
 			slog.Int("status", c.Response().StatusCode()),
 			slog.Duration("duration", time.Since(start)),
 			slog.String("ip", c.IP()),
-		)
+		}
+
+		if user, ok := helper.CurrentUser(c); ok {
+			attrs = append(attrs,
+				slog.Int("user_id", user.UserID),
+				slog.String("role", user.Role))
+		}
+		logger.Info("http_request", attrs...)
 		return err
 	}
 }
